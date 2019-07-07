c753612a93b08c181a5ff58dc30eae291ad458ca
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-07-06 21:09:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# isure-stepper
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/isure-stepper)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import 'hammerjs';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './views/home/home.component';
+
+//Material Design
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatStepperModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
