c048fda280f32330851a220a3d2ede2c4a2f47c4
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-07-06 21:53:31 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
+    MatInputModule
     ],
   declarations: [ AppComponent, HelloComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
